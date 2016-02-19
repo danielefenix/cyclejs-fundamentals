55749d02212492d6adb570a737a94a2c3d461a4c
Daniele Salvatore <daniele.salvatore@fao.org>
2016-02-19 16:00:44 +0100

    Read effects from the DOM: click events

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 //Timer
 
+//source: input (read) effects
+//sinks: output (write) effects
+
 //Logic (functional)
-function main () {
+function main (DOMSource) {
+
+  const click$ = DOMSource;
+
   return {
-    DOM : Rx.Observable.timer(0, 1000) // 0---1---2
-    .map(function(i) { return "Second elapsed " + i ; }),
-    Log: Rx.Observable.timer(0, 2000)
-    .map(function (i) { return 2 * i ;})
+    DOM : click$
+      .startWith(null) //start with null click
+      .flatMapLatest(function () {
+        return Rx.Observable.timer(0, 1000).map(function(i) { return "Second elapsed " + i ; })
+      }),
+    Log: Rx.Observable.timer(0, 2000).map(function (i) { return 2 * i ;})
   }
 }
 
@@ -16,6 +24,8 @@ function DOMDriver ( text$ ) {
     const container = document.querySelector("#timer");
     container.textContent = text;
   });
+  const DOMSource = Rx.Observable.fromEvent(document, 'click');
+  return DOMSource;
 }
 
 function consoleLogDriver( msg$ ) {
@@ -24,12 +34,24 @@ function consoleLogDriver( msg$ ) {
   })
 }
 
-function run(mainFn, effects) {
-  const sinks = mainFn();
-  //loop through the keyset of effect and pass the correspondent logic
-  Object.keys(effects).forEach(function (key) {
-    effects[key](sinks[key]);
+// bProxy = ...
+// a = f(bProxy)
+// b = g(a)
+// bProxy.imitate(b)
+
+function run(mainFn, drivers) {
+  const proxyDOMSource = new Rx.Subject();
+  const sinks = mainFn(proxyDOMSource);
+  const DOMSource = drivers.DOM(sinks.DOM);
+
+  DOMSource.subscribe(function (click) {
+    proxyDOMSource.onNext(click);
   })
+
+  //loop through the keyset of effect and pass the correspondent logic
+  //Object.keys(drivers).forEach(function (key) {
+  //  effects[key](sinks[key]);
+  //})
 }
 
 const drivers = {

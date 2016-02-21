87f791e4767cc0f9a7392cef0934a75065844f9c
danielefenix <daniele.salvatore@fao.org>
2016-02-21 14:50:19 +0100

    An interactive counter in Cycle.js

--- js/main.js
+++ js/main.js
@@ -6,41 +6,52 @@
 //Logic (functional)
 function main (sources) {
 
-  const inputEv$ = sources.DOM.select('.field').events('input');
-  const name$ = inputEv$.map(function(ev){ return ev.target.value; }).startWith("World");
+    //Hello world
+    const inputEv$ = sources.HELLO_WORLD.select('.field').events('input');
+    const name$ = inputEv$.map(function(ev){ return ev.target.value; }).startWith("World");
 
-  const sinks =  {
-    DOM : name$.map(
-      function (name) {
-        return  CycleDOM.div([
-                CycleDOM.label('Name'),
-                CycleDOM.input('.field', {type: 'input'}),
-                CycleDOM.hr(),
-                CycleDOM.h1("Hello " + name+ "!")
-            ])
-          })
-  }
+    //Counter
+    const decrementClick$ = sources.COUNTER.select('.decrement').events('click');
+    const incrementClick$ = sources.COUNTER.select('.increment').events('click');
+    const decremenetAction$ = decrementClick$.map(function () { return -1});
+    const incremenetAction$ = incrementClick$.map(function () { return +1});
+    const number$ = Rx.Observable.of(0)
+        .merge(decremenetAction$)
+        .merge(incremenetAction$)
+        .scan( function (prev, curr) { return prev + curr; }); //keep state with prev (previous value)
+
+
+    const sinks =  {
+
+      HELLO_WORLD : name$.map(
+          function (name) {
+            return  CycleDOM.div([
+                        CycleDOM.label('Name'),
+                        CycleDOM.input('.field', {type: 'input'}),
+                        CycleDOM.p([
+                            CycleDOM.label("Hello " + name+ "!")
+                        ])
+                    ])
+              }),
+      COUNTER: number$.map(function (number) {
+          return CycleDOM.div([
+                      CycleDOM.button('.decrement', 'Decrement'),
+                      CycleDOM.button('.increment', 'Increment'),
+                      CycleDOM.p([
+                          CycleDOM.label(String(number))
+                      ])
+                  ])
+        })
+  };
 
   return sinks;
 }
 
 //Effects (imperative)
-function consoleLogDriver( msg$ ) {
-  msg$.subscribe(function (text) {
-    console.log(text);
-  });
-
-}
-
-// bProxy = ...
-// a = f(bProxy)
-// b = g(a)
-// bProxy.imitate(b)
-
 const drivers = {
-  DOM: CycleDOM.makeDOMDriver("#timer"),
-  Log: consoleLogDriver
-}
+    HELLO_WORLD: CycleDOM.makeDOMDriver("#hello"),
+    COUNTER : CycleDOM.makeDOMDriver("#counter")
+};
 
 //Run app
 Cycle.run(main, drivers);
